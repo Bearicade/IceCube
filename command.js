4306ea630689ce0df7f433c62a19408e3cd64585
Alattice <ahykumin@gmail.com>
2018-05-01 21:17:58 -0400

    jokes are back online

--- command.js
+++ command.js
@@ -1,39 +1,45 @@
-module.exports = function() {
-  this.runCommand = function(prefix, client, msg, cmd) {
+const media = require("./media.json");
+module.exports = function () {
+    this.runCommand = function (prefix, client, msg, cmd) {
 
-    //Start comparing and analyzing commands
-    //Switch cases are temporary; implement only small processes
-    switch (cmd)  {
+        //Start comparing and analyzing commands
+        //Switch cases are temporary; implement only small processes
+        switch (cmd) {
 
-      //For fun, text responses
-      case 'ping':
-        msg.channel.send("Pong!");
-      break;
+            //For fun, text responses
+            case 'ping':
+                msg.channel.send("Pong!");
+                break;
 
-      //Destroy the bot, but not really
-      case 'destroy':
-        msg.channel.send("Bye bye!");
-        client.destroy();
-      break;
+                //Destroy the bot, but not really
+            case 'destroy':
+                msg.channel.send("Bye bye!");
+                client.destroy();
+                break;
 
-      //Disconnecting the client
-      case 'exit':
-        msg.channel.send("Going to sleep...");
-        client.destroy();
-      break;
+                //Disconnecting the client
+            case 'exit':
+                msg.channel.send("Going to sleep...");
+                client.destroy();
+                break;
 
-      case 'list':
-        msg.channel.send("```These are my current commands:\n" +
+            case 'list':
+                msg.channel.send("```These are my current commands:\n" +
 
-        "```");
-      break;
+                "```");
+                break;
 
-      //Default reply, unrecognized command
-      default:
-        msg.channel.send("Sorry, I don't know that command. " +
-        "Type `" + prefix + "list` for a list of commands.");
+            case 'joke':
+                msg.channel.send(media.jokes[(Math.floor(Math.random() * (media.jokes.length-1)) 
+                    )]);
+                break;
+
+                //Default reply, unrecognized command
+            default:
+                msg.channel.send("Sorry, I don't know that command. " +
+                "Type `" + prefix + "list` for a list of commands.");
+        }
     }
-}
 
 
 }
