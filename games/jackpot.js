ca22dc7c0853a03e4f3619935451d69ebc41272a
Alattice <ahykumin@gmail.com>
2018-08-03 01:43:09 -0400

    jackpot works with test style user files

--- games/jackpot.js
+++ games/jackpot.js
@@ -5,28 +5,51 @@ const fs = require("fs");
 playJackpot = function(msg, cmd, args){
 		console.log('Jackpot!');
 		var bet = parseInt(args[0]);
-		fs.readFile("./userdata/"+msg.member.id+".txt", 'utf8', function(err, data) {
-			if (err) throw err;
-			console.log(data);
-		});
-		var num1 = Math.ceil(Math.random() * 12);
-		var num2 = Math.ceil(Math.random() * 12);
-		var num3 = Math.ceil(Math.random() * 12);
-		msg.channel.send(`${msg.member} rollss | `+num1+" | "+num2+" | "+num3+" | ");
-		if(num1==num2==num3){
-			msg.channel.send("You win!\r\nYou earned "+bet+" badger coins!");
-			bet= bet;
-		}else{
-			msg.channel.send("You lost "+bet+" badger coins!");
-			bet= -bet;
-		}
+		var bank;
 		var gamefile = fs.open("./userdata/"+msg.member.id+".txt", 'a', (err, fd) => {
-			if (err) throw err;
-			fs.writeFile(fd, bet+"\r\n", 'utf8', (err) => {
-				fs.close(fd, (err) => {
-					if (err) throw err;
-				});
+			if (err) {
+				if (err.code === 'ENOENT') {
+					console.error('myfile does not exist');
+					return;
+				}
+				throw err;
+			}
+			fs.readFile("./userdata/"+msg.member.id+".txt", 'utf8', function(err, data) {
 				if (err) throw err;
+				//console.log("dat:"+data);
+				if (isNaN(parseInt(data))){
+					var bank = 100;
+				}else{
+					var bank = parseInt(data);
+				}
+				var num1 = Math.ceil(Math.random() * 3);
+				var num2 = Math.ceil(Math.random() * 3);
+				var num3 = Math.ceil(Math.random() * 3);
+				msg.channel.send(`${msg.member} rollss | `+num1+" | "+num2+" | "+num3+" | ");
+				if(num1==num2 && num2==num3){
+					msg.channel.send("You win! You earned "+bet*2+" badger coins!");
+					bet= bet*2;
+				}else{
+					msg.channel.send("You lost "+bet+" badger coins!");
+					bet= -bet;
+				}
+		
+				bank = bank + bet;
+				//console.log("bank:"+bank);
+				//console.log("bet:"+bet);
+				msg.channel.send("You have "+bank+" badger coins now.");
+		
+		
+			
+				fs.truncate("./userdata/"+msg.member.id+".txt", 0, function() {
+					fs.writeFile(fd, bank+"\r\n", 'utf8', (err) => {
+						fs.close(fd, (err) => {
+							if (err) throw err;
+						});
+						if (err) throw err;
+					});
+				});
+		
 			});
 		});
 }
